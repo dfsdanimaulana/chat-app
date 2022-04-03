7f6aae6879e0f7a5fe2a02d9996a495fa1b1d3fe
dfsdanimaulana <danimaulana9f@gmail.com>
2022-04-04 01:50:54 +0700

    login page ui done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,23 @@
 import { Link } from 'react-router-dom'
+
+// style
 import './Login.scss'
 
 export default function Login() { 
   return (
     <div className="login">
-      <h1 className="my-3">DanApp</h1>
+      <h1 className="heading heading-lg my-lg">DanApp</h1>
       <div className="social">
-        <button className="login__button btn btn-white">Sign in with Google</button>
-        <button className="login__button btn btn-white">Sign in with Facebook</button>
-        <button className="login__button btn btn-white">Sign in with Apple</button>
-        <p className='social__divider mb-md text-center'>or</p>
-        <button className="login__button btn btn-white">Sign in with Username</button>
+        <button className="btn btn-white text-start"><i className="bi bi-google mr-sm"></i> Sign in with Google</button>
+        <button className="btn btn-white text-start"><i className="bi bi-facebook mr-sm"></i> Sign in with Facebook</button>
+        <button className="btn btn-white text-start"><i className="bi bi-apple mr-sm"></i> Sign in with Apple</button>
+        <p className='social__divider'>or</p>
+        <button className="btn btn-white text-center"><i className="bi bi-person-fill mr-sm"></i> Sign in with Username</button>
       </div>
-      <p className='text my-md'>Don't have an account?</p>
-      <Link to="/signup">Signup here</Link>
+      <div className="mt-md mb-auto text-center">
+        <p className='my-sm fw-9'>Don't have an account?</p>
+        <Link to="/signup" className="fw-9 text-link">Signup here</Link>
+      </div>
     </div>
     ) 
 }
