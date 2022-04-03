d05d1fe3884198e719298d0574083b4af554ad7d
dfsdanimaulana <danimaulana9f@gmail.com>
2022-04-03 16:31:41 +0700

    add utilities

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './Login.scss'
 
 export default function Login() { 
@@ -5,10 +6,14 @@ export default function Login() {
     <div className="login">
       <h1 className="my-3">DanApp</h1>
       <div className="social">
-        <button className="btn btn-white">Sign in with Facebook</button>
-        <button className="btn btn-white">Sign in with Facebook</button>
-        <button className="btn btn-white">Sign in with Facebook</button>
+        <button className="login__button btn btn-white">Sign in with Google</button>
+        <button className="login__button btn btn-white">Sign in with Facebook</button>
+        <button className="login__button btn btn-white">Sign in with Apple</button>
+        <p className='social__divider mb-md text-center'>or</p>
+        <button className="login__button btn btn-white">Sign in with Username</button>
       </div>
+      <p className='text my-md'>Don't have an account?</p>
+      <Link to="/signup">Signup here</Link>
     </div>
     ) 
 }
